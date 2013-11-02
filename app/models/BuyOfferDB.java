398f8ed31ba1b46c9a9a2690e79570784058530c
Eduard Gamiao <edgamiao@gmail.com>
2013-11-01 23:37:33 -1000

    BuyOfferDB Compile error fixed.

--- app/models/BuyOfferDB.java
+++ app/models/BuyOfferDB.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import views.formdata.BuyOfferFormData;
 
 /**
  * A database of BuyOffers.
@@ -11,7 +12,7 @@ public class BuyOfferDB {
   private static List<BuyOffer> buyOffers = new ArrayList<BuyOffer>();
   
   public static BuyOffer addBuyOffer(BuyOfferFormData formData) {
-
+    return null;
   }
 
 }
