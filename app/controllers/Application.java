3e7304f2acd584ae86b898f641b28cfa95ec83af
Eduard Gamiao <edgamiao@gmail.com>
2013-11-01 22:04:34 -1000

    Page1 cleanup.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,6 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
-import views.html.Page1;
 
 /**
  * Implements the controllers for this application.
@@ -18,12 +17,4 @@ public class Application extends Controller {
     return ok(Index.render("Welcome to the home page."));
   }
   
-  /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
-   */
-  public static Result page1() {
-    return ok(Page1.render("Welcome to Page1."));
-    
-  }
 }
