18af67f64238b221f94a2e4d57c00c520abe118d
Eduard Gamiao <edgamiao@gmail.com>
2013-11-03 12:45:50 -1000

    Textbook intialization done.

--- app/Global.java
+++ app/Global.java
@@ -36,15 +36,39 @@ public class Global extends GlobalSettings {
     TextbookDB.addTextbook(new TextbookFormData("Data Structures: Abstraction and Design Using Java", 
         "Elliot Koffman, Paul A. T. Wolfgang", "0470128704", "Fair"));
     
-    TextbookDB.addTextbook(new TextbookFormData("Discrete Mathematics and Its Applications", "Kenneth Rosen",
-        "0073383090", "Good"));
+    TextbookDB.addTextbook(new TextbookFormData("C Programming Language", "Brian Kernighan, Dennis Ritchie",
+        "0131103628", "Poor"));
     
-    TextbookDB.addTextbook(new TextbookFormData("Discrete Mathematics and Its Applications", "Kenneth Rosen",
-        "0073383090", "Good"));
+    TextbookDB.addTextbook(new TextbookFormData("C++ Primer Plus", "Stephen Prata",
+        "0321776402", "Excellent"));
     
-    TextbookDB.addTextbook(new TextbookFormData("Discrete Mathematics and Its Applications", "Kenneth Rosen",
-        "0073383090", "Good"));
+    TextbookDB.addTextbook(new TextbookFormData("Programming Language Pragmatics", "Michael L. Scott",
+        "0123745144", "Good"));
 
+    TextbookDB.addTextbook(new TextbookFormData("Land of Lisp: Learn to Program in Lisp, One Game at a Time!", 
+        "Conrad Barski", "1593272812", "Fair"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("Database Systems: The Complete Book", 
+        "Hector-Garcia Molina, Jeffery D. Ullman, Jennifer Widom", "0131873253", "Poor"));
+
+    TextbookDB.addTextbook(new TextbookFormData("Operating System Concepts", 
+        "Abraham Silberschatz, Peter B. Galvin, Greg Gagne", "1118063333", "Excellent"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("Ethics for the Information Age", 
+        "Mike Quinn", "0132855534", "Good"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("Emotional Design: Why We Love (or Hate) Everyday Things", 
+        "Don Norman", "0465051367", "Fair"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("Presenting to Win: The Art of Telling Your Story, Updated and Expanded"
+        + " Edition", "Jerry Weissman", "0137144172", "Excellent"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("The Design of Everyday Things", 
+        "Don Norman", "0465067107", "Poor"));
+    
+    TextbookDB.addTextbook(new TextbookFormData("Artifical Intelligence for Games", 
+        "Ian Millington", "0123747317", "Good"));
+    
     TextbookDB.addTextbook(new TextbookFormData("Introduction to Algorithms", "Thomas Cormen", "0262033844", "Fair"));
 
     TextbookDB.addTextbook(new TextbookFormData("Digital Logic Design: A Rigorous Approach", "Guy Even and Moti "
